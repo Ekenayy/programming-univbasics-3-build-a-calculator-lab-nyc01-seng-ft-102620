fafd8050d016551f02ac3a605c4f8aae01b1fc7b
Ekene N <ekenayy@gmail.com>
2020-10-05 15:53:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,12 +9,12 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  answer = num1 * num2
   answer
 end
 
 def multiplication(num1, num2)
-
+  answer = num1 * num2
+  answer
 end
 
 def modulo(num1, num2)
